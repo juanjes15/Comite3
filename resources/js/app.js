c79f1300509e1a38f3a23f9049930eadf2dc3ae9
Maria Alejandra <magonzalez4826@misena.edu.co>
2023-07-28 22:08:52 -0500

    En esta nueva version dejè lista la interfaz principal, añadiendole el diseño completo para el manejo de fotos,para ello manejè swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,30 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import 'owl.carousel';
+
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
 
 Alpine.start();
+
+// En tu archivo resources/js/app.js o en otro archivo JavaScript que se cargue en la página.
+document.addEventListener('DOMContentLoaded', function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+});
