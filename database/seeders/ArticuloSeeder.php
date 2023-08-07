6c4bd98046eeead8d4f44be70ee880d893c1a663
Santiago Quintero <ensantyquintero@gmail.com>
2023-08-06 19:41:36 -0500

    Subiendo seed

---
+++ database/seeders/ArticuloSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class ArticuloSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('articulos')->insert([
+            'art_descripcion' => 'Artículo 8
+            8. Deberes del aprendiz SENA. Se entiende por deber, la obligación legal, social y moral que
+            compromete a la persona a cumplir con determinada actuación, asumiendo con responsabilidad todos
+            sus actos, para propiciar la armonía, el respeto, la integración, el bienestar común, la sana convivencia,
+            el servicio a los demás, la seguridad de las personas y de los bienes de la institución.',
+        ]);
+
+        DB::table('articulos')->insert([
+            'art_descripcion' => 'Artículo 9 
+            Prohibiciones. Se considerarán prohibiciones para los Aprendices del SENA
+            ',
+        ]);
+
+        
+           
+    }
+}
